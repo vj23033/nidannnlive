bb33c358236c0374b5cf73eb82741189a73ed752
vj23033 <162152714+vj23033@users.noreply.github.com>
2024-04-24 23:06:32 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,62 @@
+export const testimonials = [
+    {
+        name : "Sarah Thompson",
+        role : "Creative Director, PixelCraft",
+        remarks : "Sparkle Mono has revolutionized my web design process! The high fidelity wireframe sections are not only visually stunning but also incredibly easy to integrate. It has elevated the overall aesthetics of my projects",
+        avatar : "/dummyAvatar1.jpg"
+    },
+    {
+        name : "Mark Johnson",
+        role : "Creative Director, PixelCraft",
+        remarks : "“Sparkle Mono has become an essential part of our design toolkit. The wireframes are not only beautiful but also highly customizable!”",
+        avatar : "/dummyAvatar1.jpg"
+    },
+    {
+        name : "Megan Carter",
+        role : "Creative Director, PixelCraft",
+        remarks : "Sparkle Mono has become my secret weapon for client presentations. The high fidelity wireframes impress clients, helping them visualize the final product and making the approval process smoother.",
+        avatar : "/dummyAvatar1.jpg"
+    },
+    {
+        name : "Jessica Walker",
+        role : "Creative Director, PixelCraft",
+        remarks : "I've tried other wireframing tools, but Sparkle Mono stands out for its elegance and simplicity. It has streamlined our prototyping process",
+        avatar : "/dummyAvatar1.jpg"
+    },
+    {
+        name : "Jessica Walker",
+        role : "Creative Director, PixelCraft",
+        remarks : "I've tried other wireframing tools, but Sparkle Mono stands out for its elegance and simplicity. It has streamlined our prototyping process",
+        avatar : "/dummyAvatar1.jpg"
+    },
+    {
+        name : "Alex Rodriguez",
+        role : "Creative Director, PixelCraft",
+        remarks : "As a web developer, Sparkle Mono is a game-changer. The pre-made wireframes have saved me countless hours, allowing me to focus more on functionality and less on design. Truly a time-saving gem!",
+        avatar : "/dummyAvatar1.jpg"
+    },
+    {
+        name : "Kevin Lee",
+        role : "Creative Director, PixelCraft",
+        remarks : "We improved our collaboration with clients. The clear and beautiful wireframes help bridge the communication gap, making it easier for clients to understand our design concepts.",
+        avatar : "/dummyAvatar1.jpg"
+    },
+    {
+        name : "Emily Chang",
+        role : "Creative Director, PixelCraft",
+        remarks : "I was skeptical at first, but Sparkle Mono exceeded my expectations. The variety of sections is impressive, and the attention to detail is unmatched. It's like having a design team at your fingertips",
+        avatar : "/dummyAvatar1.jpg"
+    },
+    {
+        name : "JKevin Lee",
+        role : "Creative Director, PixelCraft",
+        remarks : "Sparkle Mono is a must-have for any designer or developer. The time saved on creating stunning wireframes is invaluable, allowing us to focus on delivering exceptional user experiences..",
+        avatar : "/dummyAvatar1.jpg"
+    },
+    {
+        name : "Ryan Miller",
+        role : "Creative Director, PixelCraft",
+        remarks : "The quality of Sparkle Mono's wireframes is unmatched. They have added a level of professionalism to our projects that was missing before.",
+        avatar : "/dummyAvatar1.jpg"
+    },
+]
